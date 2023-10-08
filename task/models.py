873d86cbbe5fc0039e6a6c5cccda130785c378ae
Michal <nazwakontajuzzajeta@interia.pl>
2023-10-08 15:24:50 +0200

    add picture

--- task/models.py
+++ task/models.py
@@ -12,6 +12,16 @@ class Users(models.Model):
         return f'{self.name} {self.surname}'
 
 
+class Profiles(models.Model):
+    user_id = models.ForeignKey(Users, on_delete=models.CASCADE)
+    nick = models.CharField(max_length=10)
+    # image = models.ImageField(default='default.jpg', upload_to='pictures')
+    email = models.EmailField(max_length=254)
+
+    def __str__(self):
+        return self.nick
+
+
 class ToDoLists(models.Model):
     name = models.CharField(max_length=50)
     user_id = models.ForeignKey(Users, on_delete=models.CASCADE)
@@ -20,14 +30,20 @@ class ToDoLists(models.Model):
         return self.name
 
 
+class Status(models.Model):
+    name = models.CharField(max_length=20)
+
+    def __str__(self):
+        return self.name
+
+
 class Task(models.Model):
-    ###dołożyć klucz obcy ###
     list_id = models.ForeignKey(ToDoLists, on_delete=models.CASCADE, null=True)
     name = models.CharField(max_length=50)
     description = models.TextField()
     deadline = models.DateTimeField()
     #Zmienić na oddzielną tabelkę statusów
-    status = models.CharField(max_length=32)
+    status = models.TextField(Status.name)
     finished = models.BooleanField(null=True)
 
     #wiedzieć kto i o której dodał
