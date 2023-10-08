ec6fb986cdb82ecbd8289dacb15722c4f6a91ae7
Michal <nazwakontajuzzajeta@interia.pl>
2023-10-08 12:01:06 +0200

    delete main

--- task/models.py
+++ task/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Task(models.Model):
+    name = models.CharField(max_length=50)
+    description = models.TextField()
+    deadline = models.DateTimeField()
+    #Zmienić na oddzielną tabelkę statusów
+    status = models.CharField(max_length=32)
+
+    #wiedzieć kto i o której dodał
+    added = models.DateTimeField(auto_now_add=True)
