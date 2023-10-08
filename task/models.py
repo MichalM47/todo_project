0d35ef1654fa9f74c18f41841ca45e8cf26e190d
Michal <nazwakontajuzzajeta@interia.pl>
2023-10-08 12:30:37 +0200

    delete main

--- task/models.py
+++ task/models.py
@@ -10,6 +10,8 @@ class Task(models.Model):
     deadline = models.DateTimeField()
     #Zmienić na oddzielną tabelkę statusów
     status = models.CharField(max_length=32)
+    finished = models.BooleanField(null=True)
 
     #wiedzieć kto i o której dodał
     added = models.DateTimeField(auto_now_add=True)
+
