ac955ad5a0ab4c387eb1e7da611ac0c298feb436
Michal <nazwakontajuzzajeta@interia.pl>
2023-10-09 21:27:14 +0200

    add models, views, templates to account app

--- account/views.py
+++ account/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from django.contrib.auth.views import LoginView
+from django.urls import reverse_lazy
+from django.views.generic import CreateView
+from .forms import UserCreationFormCustom
 
-# Create your views here.
+
+class LoginViewCustom(LoginView):
+    template_name = 'login.html'
+
+
+class SignUpView(CreateView):
+    template_name = 'create_account.html'
+    form_class = UserCreationFormCustom
+    success_url = reverse_lazy('index')
