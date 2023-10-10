3333f672a7b0c92ff4c349f29df8c1f20ec7b53f
Michal <nazwakontajuzzajeta@interia.pl>
2023-10-10 22:29:54 +0200

    html task details

--- task/urls.py
+++ task/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import ToDoListTemplateView
-
+from .views import ToDoListTemplateView, TasksTemplateView, DetailsTemplateView
 
 
 urlpatterns = [
     path('list/', ToDoListTemplateView.as_view(), name='list'),
+    path('tasks/', TasksTemplateView.as_view(), name='tasks_list'),
+    path('details/', DetailsTemplateView.as_view(), name='details'),
     ]
--- task/views.py
+++ task/views.py
@@ -1,8 +1,20 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView
-from .models import ToDoList
+from .models import ToDoList, Task
 
 
 class ToDoListTemplateView(TemplateView):
     template_name = 'todo_list.html'
     extra_context = {'todolist': ToDoList.objects.all()}
+
+
+class TasksTemplateView(TemplateView):
+    template_name = 'task.html'
+    extra_context = {'taskslist': Task.objects.all()}
+
+
+class DetailsTemplateView(TemplateView):
+    template_name = 'task_details.html'
+    extra_context = {'details': Task.objects.all()}
+
+
