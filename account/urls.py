19485744ce47fcbf84907a098d3dadaaf384887c
Michal <nazwakontajuzzajeta@interia.pl>
2023-10-11 22:09:06 +0200

    add main template and buttons, login redirect change in settings

--- account/urls.py
+++ account/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path, include
 from .views import LoginViewCustom, SignUpView
+from django.contrib.auth.views import LogoutView
 
 
 urlpatterns = [
     path('login/', LoginViewCustom.as_view(), name='login'),
-    path('sign-up/', SignUpView.as_view(), name='sign_up')
+    path('sign-up/', SignUpView.as_view(), name='sign_up'),
+    path('logout/', LogoutView.as_view(), name='logout'),
 
 ]
