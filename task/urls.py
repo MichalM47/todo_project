d7ea8bf3b57b01f7e24d8fcdde9f902e729f6577
Michal <nazwakontajuzzajeta@interia.pl>
2023-10-21 11:46:22 +0200

    explained problem, new view not generic

--- task/urls.py
+++ task/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
-from .views import ToDoListTemplateView, TasksTemplateView, DetailsTemplateView, MainTemplateView
+from .views import ToDoListTemplateView, DetailsTemplateView, MainTemplateView, TasksListView
 
 
 urlpatterns = [
     path('list/', ToDoListTemplateView.as_view(), name='list'),
-    path('tasks/', TasksTemplateView.as_view(), name='tasks_list'),
+    path('tasks/<int:list_id>/', TasksListView.as_view(), name='tasks_list'),
     path('details/', DetailsTemplateView.as_view(), name='details'),
     path('', MainTemplateView.as_view(), name='main'),
     ]
--- task/views.py
+++ task/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse
+from django.views import View
 from django.views.generic import TemplateView
-from .models import ToDoList, Task
+from task.models import (ToDoList, Task)
 
 
 class ToDoListTemplateView(TemplateView):
@@ -8,9 +9,9 @@ class ToDoListTemplateView(TemplateView):
     extra_context = {'todolist': ToDoList.objects.all()}
 
 
-class TasksTemplateView(TemplateView):
-    template_name = 'task.html'
-    extra_context = {'taskslist': Task.objects.all()}
+# class TasksTemplateView(TemplateView):
+#     template_name = 'task.html'
+#     extra_context = {'taskslist': Task.objects.all()}
 
 
 class DetailsTemplateView(TemplateView):
@@ -20,6 +21,32 @@ class DetailsTemplateView(TemplateView):
 
 class MainTemplateView(TemplateView):
     template_name = 'main.html'
+
+
+# class TasksListView(View):
+#
+#     def get(self, request, list_id):
+#         to_do_list = ToDoList.objects.filter(id=list_id)
+#
+#         return HttpResponse(str(to_do_list))
+
+class TasksListView(View):
+
+    def get(self, request, list_id):
+        tasks_list = Task.objects.filter(list_id_id=list_id).values_list('name')
+        print(tasks_list)
+
+        # return HttpResponse(f'Hello {str(tasks_list)}')
+        return render(request, template_name='task.html', context={'tasks':tasks_list})
+
+
+# class TasksListView(View):
+#
+#     def get(self, request, list_id):
+#         tasks_list = list(Task.objects.filter(list_id_id=list_id).values_list('name'))
+#
+#         # return HttpResponse(f'Hello {str(tasks_list)}')
+#         return render(request, template_name='task.html', context={'tasks':tasks_list})
 
 
 
