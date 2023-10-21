44b20e4c63e31e49972884f84a0083cc80bf14ca
Michal <nazwakontajuzzajeta@interia.pl>
2023-10-21 13:19:23 +0200

    not view fo details - space problem

--- task/urls.py
+++ task/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
-from .views import ToDoListTemplateView, DetailsTemplateView, MainTemplateView, TasksListView
+from .views import ToDoListTemplateView, DetailListView, MainTemplateView, TasksListView
 
 
 urlpatterns = [
     path('list/', ToDoListTemplateView.as_view(), name='list'),
     path('tasks/<int:list_id>/', TasksListView.as_view(), name='tasks_list'),
-    path('details/', DetailsTemplateView.as_view(), name='details'),
+    path('details/<int:task_id>/', DetailListView.as_view(), name='details'),
     path('', MainTemplateView.as_view(), name='main'),
     ]
--- task/views.py
+++ task/views.py
@@ -14,9 +14,9 @@ class ToDoListTemplateView(TemplateView):
 #     extra_context = {'taskslist': Task.objects.all()}
 
 
-class DetailsTemplateView(TemplateView):
-    template_name = 'task_details.html'
-    extra_context = {'details': Task.objects.all()}
+# class DetailsTemplateView(TemplateView):
+#     template_name = 'task_details.html'
+#     extra_context = {'details': Task.objects.all()}
 
 
 class MainTemplateView(TemplateView):
@@ -33,20 +33,24 @@ class MainTemplateView(TemplateView):
 class TasksListView(View):
 
     def get(self, request, list_id):
-        tasks_list = Task.objects.filter(list_id_id=list_id).values_list('name')
-        print(tasks_list)
+        tasks_list = Task.objects.filter(list_id_id=list_id).values_list('id','name')
+        # print(tasks_list)
 
         # return HttpResponse(f'Hello {str(tasks_list)}')
         return render(request, template_name='task.html', context={'tasks':tasks_list})
 
 
-# class TasksListView(View):
-#
-#     def get(self, request, list_id):
-#         tasks_list = list(Task.objects.filter(list_id_id=list_id).values_list('name'))
-#
-#         # return HttpResponse(f'Hello {str(tasks_list)}')
-#         return render(request, template_name='task.html', context={'tasks':tasks_list})
+class DetailListView(View):
+
+    def get(self, request, task_id):
+        print("aaa")
+        details_list = Task.objects.filter(id=task_id).values_list('name')
+
+        return render(request, template_name='task_details.html', context={'details':details_list})
+        # return HttpResponse(f'Hello {str(details_list)}')
+
+
+
 
 
 
